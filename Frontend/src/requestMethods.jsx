283e55ed7ac235f395a1796d9a03f906d131e913
Anoushka Deb <147077098+anoushkaaaa2004@users.noreply.github.com>
2024-10-16 14:33:35 +0530

    Update requestMethods.jsx

--- Frontend/src/requestMethods.jsx
+++ Frontend/src/requestMethods.jsx
@@ -1,7 +1,7 @@
-// src/requestMethods.js
+
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:5000/api"; // Adjust this to your backend URL
+const BASE_URL = "http://localhost:3000/api"; 
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
