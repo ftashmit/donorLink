532754de7253949ec610e63c6ebfb1ab130e5f25
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2024-10-20 00:53:20 +0530

    fix: donate now button

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import illustration from "../assets/illustration.png";
+import { Link } from "react-scroll";
 const Hero = () => {
 	return (
 		<div className="flex justify-between items-center  mx-auto container">
@@ -6,14 +7,14 @@ const Hero = () => {
 				<span className="text-2xl mt-3 text-red-500 font-bold">
 					Donate blood, Save life!
 				</span>
-				<h1 className="text-6xl leading-normal mt-3 text-black">
-					Your <span className="text-red-500 font-bold">Link 🩸</span> to
-					a life-saving moment.
+				<h1 className="text-6xl leading-normal font-bold mt-3 text-black">
+					Your <span className="text-red-500 font-bold">Link 🩸</span> to a life
+					saving moment.
 				</h1>
 
 				<div className="flex items-center gap-10 my-10">
 					<button className="bg-red-500 p-4 w-[200px] rounded-lg text-white cursor-pointer">
-						Donate Now
+						<Link to="contact">Donate Now</Link>
 					</button>
 					<button className="bg-gray-500 p-4 w-[200px] rounded-lg text-white cursor-pointer">
 						CALL : (+125) 262 728
