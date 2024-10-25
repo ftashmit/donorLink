94adba38ada775039e6cc29c37e7856d86d9de25
Ashmit Dutta <ashmitdutta@gmail.com>
2024-10-26 04:37:28 +0530

    fix:adminlogin

--- Frontend/src/requestMethods.js
+++ Frontend/src/requestMethods.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:8000/api/v1/prospect";
+const BASE_URL = "http://localhost:8000/api/v1/";
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL
